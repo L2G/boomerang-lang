b28925e1e1726d2b6da2b5684df1773f1b271306
Benjamin C. Pierce <bcpierce>
2005-06-03 19:43:51 +0000

    Checkpoint

--- examples/Common.mk
+++ examples/Common.mk
@@ -1,14 +1,5 @@
 # This is the common makefile for all the Harmony examples
 
-TOP = ../..
-
-HARMONYSRC = $(TOP)/src
-
-LENSLIB = $(TOP)/src/lib/lenses
-LENSPATH = -I $(LENSLIB) -I .
-
-HARMONY = $(HARMONYSRC)/harmony $(HARMONYFLAGS) $(LENSPATH)
-
 all: test
 
 buildandtest: build test
