80aad4f82c9de7202bb2d6615a24ce10a2b92d00
Benjamin C. Pierce <bcpierce>
2005-06-13 16:30:19 +0000

    Prelude.explode/implode and split/unsplit moved to List module.  Also, split/unsplit renamed to lines/unlines.

--- lenses/prelude.ml
+++ lenses/prelude.ml
@@ -870,26 +870,26 @@ let explode_lib = L (explode)
 let _ = register_native explode_qid "lens" explode_lib
     
 (*********)
-(* SPLIT *)
+(* LINES *)
 (*********)
-let split_qid = "Native.Prelude.split"
-let split =
+let lines_qid = "Native.Prelude.lines"
+let lines =
    { get = (fun c ->
               if not (V.is_value c) then
-                error [`String ("Native.Prelude.split (get) : expecting exactly one child :"); `View c];
+                error [`String ("Native.Prelude.lines (get) : expecting exactly one child :"); `View c];
               V.structure_from_list (Safelist.map V.new_value (Misc.split_nonescape '\n' (V.get_value c))));
      put = (fun a c ->
              let lines = Safelist.map V.get_value (V.list_from_structure a) in
              if lines=[] then
-               error [`String ("Native.Prelude.split (put) : abstract argument must be non-empty :"); `View a];
+               error [`String ("Native.Prelude.lines (put) : abstract argument must be non-empty :"); `View a];
              Safelist.iter
                (fun s -> if String.contains s '\n' then
-                           error [`String ("Native.Prelude.split (put): abstract view contains '\\n'"); `View a])
+                           error [`String ("Native.Prelude.lines (put): abstract view contains '\\n'"); `View a])
                lines;
              V.new_value (String.concat "\n" lines)) }
       
-let split_lib = L (split)
-let _ = register_native split_qid "lens" split_lib
+let lines_lib = L (lines)
+let _ = register_native lines_qid "lens" lines_lib
     
 (* PAD *)
 (* pad a list to a power of two *)
