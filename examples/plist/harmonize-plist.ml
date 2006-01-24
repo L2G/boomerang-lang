cc4636c73782742cc38800d273aa8c8e4ae46460
Benjamin C. Pierce <bcpierce>
2006-01-24 04:44:57 +0000

    More better version

--- examples/plist/harmonize-plist.ml
+++ examples/plist/harmonize-plist.ml
@@ -1,3 +1,9 @@
+(* FIX: It's a shame that these definitions occur both here and in the safari
+   synchronizer (and they will occur in every other synchronizer that uses plists!). 
+   They should be broken out into their own little module -- the only question is
+   where it should live. (Not in /src, I think.)  --B *)
+
+let recompressPlists = Prefs.createBool "compress" false "convert plist files to binary form after sync" ""
 
 let plutil f fpre =
   Toplevel.runcmd (Printf.sprintf "plutil -convert xml1 %s -o %s" (Misc.whack f) (Misc.whack fpre))
@@ -9,7 +15,7 @@ let plutilback f fpost =
     Toplevel.runcmd (Printf.sprintf "cp %s %s" (Misc.whack f) (Misc.whack fpost))
   
 let chooseEncoding f =
-  if Util.endswith f ".plist" then ("xml",Safari,Some plutil,Some plutilback)
+  if Util.endswith f ".plist" then ("xml",(),Some plutil,Some plutilback)
   else raise Not_found
 
 let chooseAbstractSchema types = "Plist.T"
