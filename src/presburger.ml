64016d9c65ee1d40f381da523675a9e31b99e450
Nate Foster <jnfoster>
2006-04-03 11:34:14 +0000

    fixed presburger module, standardized omega makefile to build and clean correctly, added oc to src/Makefile

--- src/presburger.ml
+++ src/presburger.ml
@@ -14,129 +14,15 @@ module IntSet = Set.Make(
 
 (* -------------- abstract syntax ------------- *)
 type exp = 
-    Const of int
-  | Var of int 
-  | Sum of exp * exp
+    Const of Info.t * int
+  | Var of Info.t * int 
+  | Sum of Info.t * exp * exp
+(* formulas are represented in de Bruijn notation *)
 and formula = 
     Equal of Info.t * exp * exp 
   | Not of Info.t * formula
   | Or of Info.t * formula * formula
   | Exists of Info.t * formula
-
-(* calculate the free variables of an exp or formula *)
-let rec fvs =
-  let rec fvs_exp = function
-      Const(_)   -> IntSet.empty
-    | Var(x)     -> IntSet.singleton x
-    | Sum(e1,e2) -> IntSet.union (fvs_exp e1) (fvs_exp e2) 
-  in function
-        Equal(_,e1,e2) -> IntSet.union (fvs_exp e1) (fvs_exp e2)
-    | Not(_,f)       -> fvs f
-    | Or(_,f1,f2)    -> IntSet.union (fvs f1) (fvs f2)
-    | Exists(_,f)    -> IntSet.fold 
-        (fun x s -> if (x = 0) then vs else IntSet.add (x+1) vs)
-          (fvs f)        
-
-(* --------------- de Bruijn shifting -------------- *)
-let shift n = 
-  let shift_exp_aux n c = function`
-      Const(n) -> Const(n) 
-    | Var(x) -> if x < c then x else x + n
-    | Sum(e1,e2) -> Sum(shift_exp_aux n c e1, shift_exp_aux n c e2)
-  in
-  let shift_aux n c = function 
-      Equal(i,e1,e2) -> Equal(i, shift_exp_aux n c e1, shift_exp_aux n c e2)
-    | Not(i,f)       -> Not(i,shift_aux n c f)
-    | Or(i,f1,f2)    -> Or(i, shift_aux n c f1, shift_aux n c f2)
-    | Exists(i,f)    -> Exists(i, shift_aux n (c+1) f)
-  in
-    shift_aux n 0
-
-(** substitute: -> exp -> int -> formula -> formula *)
-let substitute e x = 
-  let substitute_exp e x = function
-      Const(n)   -> Const(n)
-    | Var(y)     -> if (x = y) then e else e0        
-    | Sum(e1,e2) -> Sum(substitute e x e1, substitute e x e2)
-  in function
-      Equal(i,e1,e2) -> Equal(i, substitute e x f1, substitute e x f2)
-    | Not(i,f)       -> Not(i, substitute e x f)
-    | Or(i,f1,f2)    -> Or(i, substitute e x f1, substitute e x f2)
-    | Exists(i,f)    -> Exists(i, substitute e x (shift_formula 1 f)) (* FIXME: optimize this to single pass *)
-    
-(** instantiate: exp list -> formula -> formula *)
-let instantiate es f = 
-  let rec zip_trunc l1 l2 = match l1,l2 with 
-      [],_ | _,[]       -> []
-    | (h1::t1),(h2::t2) -> (h1,t2)::(t1,t2) in
-  let instantiate_exp xs_es = function
-      Const(n)   -> Const(n)
-    | Var(y)     -> begin try Safelist.assoc y xs_es with Not_found -> Var(y) end
-    | Sum(e1,e2) -> Sum(instantiate_exp xs_es e1, instantiate_exp xs_es e2) in            
-  let instantiate_aux xs_es = function
-      Equal(i,e1,e2) -> Equal(i, instantiate_exp xs_es e1, instantiate_exp xs_es e2)
-    | Not(i,f)       -> Not(i, instantiate_aux xs_es f)
-    | Or(i,f1,f2)    -> Or(i, instantiate xs_es f1, instantiate xs_es f2)
-    | Exists(i,f)    -> Exists(i, instantiate (Safelist.remove_assoc 0 xs_es) f) in
-  let xs = IntSet.elements (fvs f) in 
-  let xs_es = zip_trunc xs es in
-    instantiate_aux xs_es
-      
-(** mk_fresh_vars: int -> int -> exp list *)
-(** creates @n@ fresh variables starting from @d@ *)
-let mk_fresh_vars n d = 
-  let rec aux acc = function 
-      0 -> Safelist.rev acc 
-    | n -> aux (Var(n + d)::acc) (n-1) in
-    aux i [] n in
-
-
-(* --------------- constructors for derived forms --------------- *)
-(** mkAnd Info.t -> formula -> formula -> formula *)
-let mkAnd i f1 f2 = Not(i,Or(i,Not(i,f1),Not(i,f2)))
-
-(** mkImplies Info.t -> formula -> formula -> formula *)
-let mkImplies i f1 f2 = Or(i,Not(i,f1),f2)
-
-(** mkEquiv Info.t -> formula -> formula -> formula *)
-let mkEquivalent i f1 f2 = And(i,mkImplies(i,f1,f2),mkImplies(i,f2,f1))
-
-(** mkForall: Info.t -> formula -> formula *)
-let mkForall i f = Not(i,Exists(i,Not(i,f)))
-
-(** mkLess: Info.t -> exp -> exp -> formula *)  
-let mkLess i e1 e2 = Exists(i,Equal(shift_exp 1 e1,Sum(i,shift_exp 1 e2,Sum(i,Var(0),Sum(i,Const(1))))))
-
-(**mkStar: Info.t -> formula -> formula *)
-let mkStar i f1 = f1 (* TODO! *)
-
-(** mkSum: Info.t -> formula -> formula -> formula *)
-let mkSum i f1 f2 = 
-  let fv1 = fv_formula f1 in 
-  let fv2 = fv_formula f2 in 
-  let c1 = IntSet.cardinal fv1 in
-  let c2 = IntSet.cardinal fv2 in 
-  let _ = assert(c2 = c2) in 
-    
-  (* instantiate f1 with 0 to (c1-1) and f1 with c1 to c1+c2-1 *)
-  let f1' = instantiate (fresh_vars c1 0) f1 in
-  let f2' = instantiate (fresh_vars c2 c1) f2 in 
-    
-  let rec build_eqs f = function
-      -1 -> f
-    | i -> 
-        let s = Sum(Var(i),(Var(i+c1))) in 
-        let e = Equal(i,Var(i+c1+c2),s) in
-        let a = mkAnd i f e in
-          build_eqs a (n-1)
-  in
-  let f = build_eqs 
-    (* now just close it all up under some exists *)
-let rec build_sum n f = 
-  if (n = 0) then f
-    else Exists(i,f)
-    
-
 
 (* --------------- utility functions --------------- *)
 let info_of_exp = function
@@ -147,6 +33,130 @@ let info_of_formula = function
     Equal(i,_,_)   -> i
   | Not(i,_)       -> i
   | Or(i,_,_)      -> i
-  | Exists(i,_,_)  -> i
+  | Exists(i,_)  -> i
+
+(* calculate the free variables of an exp or formula *)
+let rec fvs =
+  let rec fvs_exp = function
+      Const(_,_)   -> IntSet.empty
+    | Var(_,x)     -> IntSet.singleton x
+    | Sum(_,e1,e2) -> IntSet.union (fvs_exp e1) (fvs_exp e2) 
+  in function
+      Equal(_,e1,e2) -> IntSet.union (fvs_exp e1) (fvs_exp e2)
+    | Not(_,f)       -> fvs f
+    | Or(_,f1,f2)    -> IntSet.union (fvs f1) (fvs f2)
+    | Exists(_,f)    -> 
+        IntSet.fold 
+          (fun x s -> if (x <> 0) then (IntSet.add (x-1) s) else s) 
+          (fvs f)
+          IntSet.empty 
+
+(* --------------- de Bruijn shifting -------------- *)
+(* cutoff *)
+let rec shift_exp_aux n c = function
+    Const(i,n)    -> Const(i,n) 
+  | Var(i,x)      -> Var(i, if (x <= c) then x else x + n)
+  | Sum(i,e1,e2)  -> Sum(i,shift_exp_aux n c e1, shift_exp_aux n c e2)
+and shift_aux n c = function 
+    Equal(i,e1,e2) -> Equal(i, shift_exp_aux n c e1, shift_exp_aux n c e2)
+  | Not(i,f)       -> Not(i,shift_aux n c f)
+  | Or(i,f1,f2)    -> Or(i, shift_aux n c f1, shift_aux n c f2)
+  | Exists(i,f)    -> Exists(i, shift_aux n (c+1) f)
+
+(* plain shifting *)
+let shift_exp n = shift_exp_aux n 0
+let shift n = shift_aux n 0
+
+(* simulatneous substitution *)
+(** substitute: -> int list -> exp list -> formula -> formula *)
+let rec substitute xs es = 
+  let rec do_subst xs es i y = 
+    match xs,es with
+        [],_ | _,[] -> Var(i,y)
+      | (x1::xt),(e1::et) -> 
+          if (y = x1) then e1 else do_subst xt et i y in
+  let rec substitute_exp xs es = function
+      Const(i,n)   -> Const(i,n)
+    | Var(i,y)     -> do_subst xs es i y
+    | Sum(i,e1,e2) -> 
+        Sum(i,substitute_exp xs es e1, substitute_exp xs es e2) in 
+    function        
+      Equal(i,e1,e2) -> Equal(i, substitute_exp xs es e1, substitute_exp xs es e2)
+    | Not(i,f)       -> Not(i, substitute xs es f)
+    | Or(i,f1,f2)    -> Or(i, substitute xs es f1, substitute xs es f2)
+    | Exists(i,f)    -> Exists(i, substitute (List.map succ xs) (List.map (shift_exp 1) es) f)
+    
+(** instantiate: exp list -> formula -> formula *)
+let instantiate es f = 
+  let rec mk_xs i = function [] -> [] | _::t -> i::(mk_xs (i+1) t) in
+    substitute (mk_xs 0 es) es f
+
+(* --------------- constructors --------------- *)
+
+(** mkConst: Info.t -> int -> exp *)
+let mkConst i n = Const(i,n)
+
+(** mkVar: Info.t -> int -> exp *)
+let mkVar i x = Var(i,x)
+
+(** mkSum: exp -> exp -> exp *)
+let mkSum e1 e2 = Sum(Info.merge_inc (info_of_exp e1) (info_of_exp e2),e1 ,e2)
+
+(** mkEqual: exp -> exp -> formula *)
+let mkEqual e1 e2 = Equal(Info.merge_inc (info_of_exp e1) (info_of_exp e2),e1,e2)
+
+(** mkNot: formula -> formula *)
+let mkNot f = Not(info_of_formula f, f)
  
-let format_t t0 = function _ -> Format.printf "UNIMPLEMENTED"
+(** mkOr: formula -> formula -> formula *)
+let mkOr f1 f2 = 
+  Or(Info.merge_inc (info_of_formula f1) (info_of_formula f2),f1,f2)
+
+(** mkExists: formula -> formula *)
+let mkExists f = Exists(info_of_formula f,f)
+
+(** mkAnd  formula -> formula -> formula *)
+let mkAnd f1 f2 =  mkNot (mkOr (mkNot f1) (mkNot f2))
+
+(** mkLt: exp -> exp -> formula *)
+let mkLt e1 e2 = 
+  mkExists
+    (mkEqual 
+       (shift_exp 1 e1)
+       (mkSum (shift_exp 1 e2) (mkVar (info_of_exp e2) 0)))
+
+(** mkLe: exp -> exp -> formula *)
+let mkLe e1 e2 = mkOr (mkEqual e1 e2) (mkLt e1 e2)
+
+(** mkGt: exp -> exp -> formula *)
+let mkGt e1 e2 = mkLt e2 e1
+
+(** mkGe: exp -> exp -> formula *)
+let mkGe e1 e2 = mkLe e2 e1
+
+(** mkSum: -> formula -> formula -> formula *)
+let mkSum_formula f1 f2 = 
+  let i = Info.merge_inc (info_of_formula f1) (info_of_formula f2) in
+  let rec fresh start num acc =     
+    if num = 0 then acc
+    else fresh (start+1) (num-1) (Var(i,start)::acc) in
+  let c1 = IntSet.cardinal (fvs f1) in
+  let c2 = IntSet.cardinal (fvs f2) in 
+  let _ = assert(c1 = c2) in 
+    
+  (* instantiate f1 with 0 to (c1-1) and f1 with c1 to c1+c2-1 *)
+  let f1' = instantiate (List.rev (fresh 0 c1 [])) f1 in
+  let f2' = instantiate (List.rev (fresh c1 c2 [])) f2 in 
+  let rec mk_sums n acc = 
+    if n = 0 then acc 
+    else 
+      mk_sums (n-1)
+        ((mkEqual 
+            (mkVar i (n+c1+c2))
+            (mkSum (mkVar i n) (mkVar i (n+c1))))::acc) in
+  let rec f' = List.fold_left (fun c cs -> mkAnd cs c) f1' (f2'::mk_sums c1 []) in
+  let rec mk_exists n acc = if n = 0 then acc else mk_exists (n-1) (mkExists acc) in             
+    mk_exists c1 f'
+      
+let rec format_exp = function _ -> Format.printf "UNFINISHED"
+let format = function _ -> Format.printf "UNFINISHED"
