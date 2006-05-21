decfe97940a57464cb20e3c41c4877eeae5daea9
Benjamin C. Pierce <bcpierce>
2006-05-21 17:19:48 +0000

    Add listexamples demo

---
+++ examples/listexamples/demos.php
@@ -0,0 +1,44 @@
+<?
+
+$demogroupname = "Lists";
+$demo["democmd"] = "../../src/harmony";
+$demo["forcer1"] = true;
+$demo["default_h"] = 200;
+$demo["schema"] = "List.T Value";
+$demo["r1format"] = $demo["r2format"] = "txt";
+$demo["l1"] = $demo["l2"] = "List.lines";
+$demo["output_d"] = "block";
+
+# ---------------------------------------------------------
+$demo["instr"] = <<<XXX
+
+Harmony's synchronization algorithm handles <i>lists</i> specially,
+using the same alignment algorithm as the popular <tt>diff3</tt>
+command-line utility (which is also used by version management systems
+such as CVS and Subversion).
+<p>
+
+Try the following on the text files below (which are being parsed as lists of lines): 
+<ol>
+<li> Change <tt>a</tt> to <tt>FOO</tt> in replica 1 and to to <tt>BAR</tt> in replica 2.
+<li> Insert a few new lines between <tt>b</tt> and <tt>c</tt> in replica 1.
+<li> Delete <tt>d</tt> and <tt>e</tt> in replica 2.
+<li> In both replicas, change <tt>g</tt> to <tt>G</tt>.
+</ol>
+
+XXX;
+# ---------------------------------------------------------
+$demo["r1"] = <<<XXX
+a
+b
+c
+d
+e
+f
+g
+XXX;
+savedemo();
+# ---------------------------------------------------------
+
+
+?>
