3b51a20ac466185cbb32766740a978e7f3548649
kkunal <kkunal>
2006-05-22 18:40:11 +0000

    modified diff3.ml to take care of printing problems

--- src/diff3.ml
+++ src/diff3.ml
@@ -93,10 +93,10 @@ let format_list l =
 
 let sync elt_schema (o,a,b) =
   debug (fun () ->
-    Printf.eprintf "Inputs:\n";
-    Printf.eprintf "          o = "; print_list o; Printf.eprintf "\n"; 
-    Printf.eprintf "          a = "; print_list a; Printf.eprintf "\n"; 
-    Printf.eprintf "          b = "; print_list b; Printf.eprintf "\n"); 
+	   Printf.eprintf "Inputs:\n";
+	   Printf.eprintf "          o = "; print_list o; Printf.eprintf "\n"; 
+	   Printf.eprintf "          a = "; print_list a; Printf.eprintf "\n"; 
+	   Printf.eprintf "          b = "; print_list b; Printf.eprintf "\n"); 
   let len_a = Safelist.length a in 
   let len_b = Safelist.length b in
   let len_o = Safelist.length o in
@@ -106,53 +106,53 @@ let sync elt_schema (o,a,b) =
   let make_comp arr_1 arr_2 = 
     (* set comp_oa [i,j] to true iff arr_o[i]=arr_o[j]... *)
     let comp = Array.make_matrix (Array.length arr_1) (Array.length arr_2) true in 
-    Array.iteri 
-      (fun i x -> 
-         comp.(i) <- 
-           ( Array.map (fun elt_a -> A.eqv elt_a x ) arr_2 ))
-      arr_1;
-    comp in
+      Array.iteri 
+	(fun i x -> 
+           comp.(i) <- 
+             ( Array.map (fun elt_a -> A.eqv elt_a x ) arr_2 ))
+	arr_1;
+      comp in
   let comp_oa = make_comp arr_o arr_a in
   let comp_ob = make_comp arr_o arr_b in
   let make_match_list comp l_o l_a =  
     let lcs_oa = Array.make_matrix (l_o+1) (l_a+1) 0 in 
     let lcs_aux = Array.make_matrix (l_o+1) (l_a+1) Diag in 
-    Array.iteri 
-      (fun i x -> 
-         if (i=0) then ()  
-         else Array.iteri 
-           (fun j y ->
-              if (j=0) then () 
-              else if comp.(i-1).(j-1) then 
-                let _ = lcs_aux.(i).(j) <- Diag in 
-                lcs_oa.(i).(j) <- lcs_oa.(i-1).(j-1) + 1 
+      Array.iteri 
+	(fun i x -> 
+           if (i=0) then ()  
+           else Array.iteri 
+             (fun j y ->
+		if (j=0) then () 
+		else if comp.(i-1).(j-1) then 
+                  let _ = lcs_aux.(i).(j) <- Diag in 
+                    lcs_oa.(i).(j) <- lcs_oa.(i-1).(j-1) + 1 
                 else if (lcs_oa.(i-1).(j) > lcs_oa.(i).(j-1)) then  
                   let _ = lcs_aux.(i).(j) <- Top in
-                  lcs_oa.(i).(j) <- lcs_oa.(i-1).(j) 
-              else
-                let _ = lcs_aux.(i).(j) <- Left in
-                lcs_oa.(i).(j) <- lcs_oa.(i).(j-1))
-           lcs_oa.(i)) 
-      lcs_oa;
-    let rec find_matches i j l = 
-      if (i=0) or (j=0) then l
-      else match lcs_aux.(i).(j) with  
-              Diag -> find_matches (i-1) (j-1)  ((i-1,j-1) :: l)  
-            | Top -> find_matches  (i-1) j  l    
-            | _ ->  find_matches  i (j-1)  l in
-    find_matches l_o l_a  [] in
+                    lcs_oa.(i).(j) <- lcs_oa.(i-1).(j) 
+		else
+                  let _ = lcs_aux.(i).(j) <- Left in
+                    lcs_oa.(i).(j) <- lcs_oa.(i).(j-1))
+             lcs_oa.(i)) 
+	lcs_oa;
+      let rec find_matches i j l = 
+	if (i=0) or (j=0) then l
+	else match lcs_aux.(i).(j) with  
+            Diag -> find_matches (i-1) (j-1)  ((i-1,j-1) :: l)  
+          | Top -> find_matches  (i-1) j  l    
+          | _ ->  find_matches  i (j-1)  l in
+	find_matches l_o l_a  [] in
   let same_lines_oa = make_match_list comp_oa len_o len_a in 
   let same_lines_ob = make_match_list comp_ob len_o len_b in
   let rec common_lines lines_oa lines_ob l =
     match lines_oa with 
-      [] -> l
-    | (lo,la)::tl -> 
-        match lines_ob with 
-          [] -> l 
-        | (lo',lb)::tl' ->
-            if lo = lo' then common_lines tl tl' ((lo,la,lb)::l)
-            else if lo > lo' then common_lines lines_oa tl'  l 
-            else common_lines tl lines_ob  l in
+	[] -> l
+      | (lo,la)::tl -> 
+          match lines_ob with 
+              [] -> l 
+            | (lo',lb)::tl' ->
+		if lo = lo' then common_lines tl tl' ((lo,la,lb)::l)
+		else if lo > lo' then common_lines lines_oa tl'  l 
+		else common_lines tl lines_ob  l in
   let same_lines_oab =
     Safelist.append (common_lines same_lines_oa same_lines_ob []) [(-1,-1,-1)] in 
   let (x, (acts,o',a',b')) =
@@ -168,10 +168,19 @@ let sync elt_schema (o,a,b) =
          let is_diff_oa = find_diff (so+1) (sa+1) eo ea comp_oa in 
          let is_diff_ob = find_diff (so+1) (sb+1) eo eb comp_ob in 
 
-         let get_lines sl el arr =
-           let isfirst = (sl = -1) in 
-           let len = if isfirst then el-sl-1 else el-sl in  
-           let start = if isfirst then 0 else sl in  
+      (* old code not needed    
+	 let get_lines sl el arr =
+         let isfirst = (sl = -1) in 
+         let len = if isfirst then el-sl-1 else el-sl in  
+         let start = if isfirst then 0 else sl in  
+         if (len > 0) then 
+         (* BCP: Next line is hideous... *)
+         Array.to_list (Array.init len (fun i -> arr.(start+i)))
+             else 
+         [] in
+      *)
+	 let get_lines sl el arr =
+           let len = if el - sl in 
              if (len > 0) then 
                (* BCP: Next line is hideous... *)
                Array.to_list (Array.init len (fun i -> arr.(start+i)))
@@ -180,80 +189,83 @@ let sync elt_schema (o,a,b) =
 
          let header s =
            Format.printf
-            "-- Elements %d-%d in archive, %d-%d in replica 1, %d-%d in replica 2: --@,"
-            (so+2) eo (sa+2) (ea) (sb+2) (eb) in
-         (* let header s =
-           Format.printf "@[<v 0>%s in the chunk consisting of" s;
-           Format.printf "@    %d lines from %d-%d in archive,"
-             (eo-so-1) (so+2) eo;
-           Format.printf "@    %d lines from %d-%d in replica 1,"
-             (ea-sa-1) (sa+2) (ea);
-           Format.printf "@    %d lines from %d-%d in replica 2...@]@,"
-             (eb-sb-1) (sb+2) (eb) in *)
+             "-- Elements %d-%d in archive, %d-%d in replica 1, %d-%d in replica 2: --@,"
+             (so+2) eo (sa+2) (ea) (sb+2) (eb) in
+           (* let header s =
+              Format.printf "@[<v 0>%s in the chunk consisting of" s;
+              Format.printf "@    %d lines from %d-%d in archive,"
+              (eo-so-1) (so+2) eo;
+              Format.printf "@    %d lines from %d-%d in replica 1,"
+              (ea-sa-1) (sa+2) (ea);
+              Format.printf "@    %d lines from %d-%d in replica 2...@]@,"
+              (eb-sb-1) (sb+2) (eb) in *)
 
          let showchange s nw ol =
            let rec list_change_lines m = function
                [] -> ()
              | [e]     -> Format.printf "%s (%s) " m s; A.format e
              | e::rest -> Format.printf "%s (%s) " m s; A.format e;
-                          Format.printf ",@,"; list_change_lines m rest in
-           if nw = [] then list_change_lines "Delete" ol
-           else if ol=[] then list_change_lines "Add" nw 
-           else list_change_lines "Change" nw 
-in
-
-         let onew = get_lines so eo arr_o in 
-         let anew = get_lines sa ea arr_a in 
-         let bnew = get_lines sb eb arr_b in  
-
-         if is_diff_oa && is_diff_ob then begin
-           let len_onew = Safelist.length onew in 
-           let len_anew = Safelist.length anew in 
-           let len_bnew = Safelist.length bnew in 
-           debug (fun() -> Printf.eprintf "o="; print_list onew;
-                           Printf.eprintf "\na="; print_list anew;
-                           Printf.eprintf "\nb="; print_list bnew;
-                           Printf.eprintf "\n");
-           if len_onew = len_anew && len_onew = len_bnew then begin
-             (* Recursively synchronize, element by element *)
-             let (subacts, onew', anew', bnew') =
-               unzip4
-                 (Safelist.map
-                    (fun (o,a,b) ->
-                       let (a, oo', oa', ob') =
-                         A.sync elt_schema (Some o,Some a,Some b) in
-                       (a, the oo', the oa', the ob'))
-                    (zip3 (onew, anew, bnew))) in
-             let act () =
-               header "Reconciling changes line by line";
-               (* Format.printf "   (Length = %d)@," len_onew; *)
-               let rec loop first = function
-                   [] -> ()
-                 | a::rest ->
-                     if not first then Format.printf ",@,";
-                     A.format_action a;
-                     loop false rest in
-               loop true subacts in
-             let hc () = Safelist.exists A.has_conflict subacts in
-             ((so,sa,sb),((Some act,hc)::acts,onew'@o',anew'@a',bnew'@b'))
+                 Format.printf ",@,"; list_change_lines m rest in
+             if nw = [] then list_change_lines "Delete" ol
+             else if ol=[] then list_change_lines "Add" nw 
+             else list_change_lines "Change" nw 
+	 in
+         let onew = get_lines so+1 eo arr_o in 
+         let anew = get_lines sa+1 ea arr_a in 
+         let bnew = get_lines sb+1 eb arr_b in  
+           
+	 (* so, eo are the matching line numbers - so the differing lines are
+	    so+1, so+2 ...eo-1  - 
+	    When finally adding the lines to reconciled version, we need to add the common line too *)
+         let common = if (sb=-1) then [] else get_lines sb+1 eb arr_b in    
+           if is_diff_oa && is_diff_ob then begin
+             let len_onew = Safelist.length onew in 
+             let len_anew = Safelist.length anew in 
+             let len_bnew = Safelist.length bnew in 
+               debug (fun() -> Printf.eprintf "o="; print_list onew;
+                        Printf.eprintf "\na="; print_list anew;
+                        Printf.eprintf "\nb="; print_list bnew;
+                        Printf.eprintf "\n");
+               if len_onew = len_anew && len_onew = len_bnew then begin
+		 (* Recursively synchronize, element by element *)
+		 let (subacts, onew', anew', bnew') =
+		   unzip4
+                     (Safelist.map
+			(fun (o,a,b) ->
+			   let (a, oo', oa', ob') =
+                             A.sync elt_schema (Some o,Some a,Some b) in
+			     (a, the oo', the oa', the ob'))
+			(zip3 (onew, anew, bnew))) in
+		 let act () =
+		   header "Reconciling changes line by line";
+		   (* Format.printf "   (Length = %d)@," len_onew; *)
+		   let rec loop first = function
+                       [] -> ()
+                     | a::rest ->
+			 if not first then Format.printf ",@,";
+			 A.format_action a;
+			 loop false rest in
+		     loop true subacts in
+		 let hc () = Safelist.exists A.has_conflict subacts in
+		   ((so,sa,sb),((Some act,hc)::acts,common@onew'@o',common@anew'@a',common@bnew'@b'))
+               end else begin
+		 let act () =
+		   header "Conflict";
+		   Format.printf "@[<hv 4>Conflict between@ "; format_list anew;
+		   Format.printf "@]@ @[<hv 4>and@ "; format_list bnew; Format.printf "@]" in
+		   ((so,sa,sb),((Some act,confl)::acts,common@onew@o',common@anew@a',common@bnew@b'))
+               end
+           end else if is_diff_oa then begin   
+             let act () = header "Replica 1 changed"; showchange "-->" anew bnew in
+               ((so,sa,sb),((Some act,noconfl)::acts,common@anew@o',common@anew@a',common@anew@b'))        
            end else begin
-             let act () =
-               header "Conflict";
-               Format.printf "@[<hv 4>Conflict between@ "; format_list anew;
-               Format.printf "@]@ @[<hv 4>and@ "; format_list bnew; Format.printf "@]" in
-             ((so,sa,sb),((Some act,confl)::acts,onew@o',anew@a',bnew@b'))
-           end
-         end else if is_diff_oa then begin   
-           let act () = header "Replica 1 changed"; showchange "-->" anew bnew in
-           ((so,sa,sb),((Some act,noconfl)::acts,anew@o',anew@a',anew@b'))        
-         end else begin
-           (* Only b is different or else all three are the same *) 
-           let act = 
-             if is_diff_ob then
-               Some (fun()-> header "Replica 2 changed"; showchange "<--" bnew anew)
-             else None in
-           ((so,sa,sb),((act,noconfl)::acts,bnew@o',bnew@a',bnew@b'))      
-         end)
+             (* Only b is different or else all three are the same *) 
+             let act = 
+               if is_diff_ob then
+		 Some (fun()-> header "Replica 2 changed"; showchange "<--" bnew anew)
+               else None in
+               ((so,sa,sb),((act,noconfl)::acts,common@bnew@o',common@bnew@a',common@bnew@b'))      
+           end)
       ((len_o,len_a,len_b),([],[],[],[]))
       same_lines_oab 
   in
