a4cf0274c13b8242fca7b9b52db1b049964de284
Benjamin C. Pierce <bcpierce>
2006-06-22 03:13:01 +0000

    Little bit more progress on relational web demos

--- examples/relational/demos.php
+++ examples/relational/demos.php
@@ -12,7 +12,7 @@ $demo["r2_title"] = "View";
 $demo["l1_title"] = "Lens";
 $demo["l1_d"] = true;
 $demo["l2_d"] = false;
-$demo["l2_title"] = "(Not needed)";
+$demo["l2_title"] = "(Not used)";
 $demo["r1_shows"] = true;
 $demo["l2"] = "id";
 $demo["output_d"] = "block";
@@ -80,16 +80,14 @@ that records the schema of the view.
 
 <p>
 
-The following pages break this lens down into its components and
-examines their behavior separately.  But to get started, let's try
+Let's try
 making a few changes to the view and seeing how they are reflected in
-the database.
-
-<ul>
-<li> ...
-</ul>
-
+the database.  
+(Go to next page.)
 XXX;
+
+# The following pages break this lens down into its components and
+# examine their behavior separately.  But to get started, 
 
 $demo["forcer1"] = true;
 $demo["l1"] = <<<XXX
@@ -101,7 +99,8 @@ assert
   }} ;
 
 (* Join Tracks with Albums to yield a new relation Tracks1 *)
-Relational.join_dl "Tracks" with {Track -> Date, Track -> Rating}
+Relational.join_dl 
+  "Tracks" with {Track -> Date, Track -> Rating}
   "Albums" with {Album -> Quantity}
   "Tracks1" ;
 
@@ -144,42 +143,45 @@ XXX;
 savedemo();
 # ---------------------------------------------------------
 $demo["instr"] = <<<XXX
-(testing)
-XXX;
 
-$demo["l1"] = <<<XXX
-Relational.join_dl "Tracks" with {Track -> Date, Track -> Rating}
-  "Albums" with {Album -> Quantity}
-    "Tracks1" 
-;
-Relational.drop "Tracks1" "Tracks2" "Date" {Track} "unknown date"
-;
-Relational.select "Tracks2" with {Album -> Quantity, Track -> Rating} "Tracks3"
-  where (Quantity <> "0" /\ (Quantity <> "1" /\ Quantity <> "2"))
-XXX;
-$demo["r1"] = <<<XXX
-  {{{
-    Albums (Album,           Quantity) = {
-           (Disintegration,  6       )
-           (Show,            3       )
-           (Galore,          1       )
-           (Paris,           4       )
-           (Wish,            5       )   }
+<ul> <li> First, try changing the rating of Lovesong in the view.
+Press the Synchronize button and notice that the new rating gets
+propagaged back to the database.  (In this set of demos, where there
+is no second lens, "synchronize" just means "push the new value from
+the view back into the database.")
 
-    Tracks (Track,    Date, Rating, Album) = {
-           (Lulluby,  1989, 3,      Galore)
-           (Lulluby,  1989, 3,      Show) 
-           (Lovesong, 1989, 5,      Galore)
-           (Lovesong, 1989, 5,      Paris)
-           (Trust,    1992, 4,      Wish)
-    }
-  }}} 
-XXX;
-$demo["r2"] = <<<XXX
-{{{ Tracks3(Track, Rating, Album, Quantity) =
-          {(Lovesong, 5, Paris, 4),
-           (Lulluby, 3, Show, 3),
-           (Trust, 4, Wish, 5)} }}}
+<li> Next try changing the quantity of Trust in the view to 1.  Notice
+that the quantity gets propagated to the database.  
+Exercise: What do
+you think will happen if you try to change the quantity to 1?
+
+<li> Delete the tuple (Trust, 4, Wish, 5) from the view and
+synchronize.  Notice that the entry for Trust is removed from the
+Tracks database.  
+
+We could also have achieved the deletion from the view by deleting the
+tuple (Wish, 5) from the Albums database; if this is the behavior we
+want, we can get it by switching the order of the arguments to the
+<tt>join_dl</tt> lens (<tt>_dl</tt> stands for "deleting from the
+left"--i.e., the first argument) so that '"Albums" with {Album ->
+Quantity}' comes before '"Tracks" with {Track -> Date, Track ->
+Rating}'.  Try this.
+
+<li> Finally, take a look at the tuple (Paris, 4, Lovesong, 5) in the
+view.  The sources of this information are the tuples (Paris, 4) in
+the Albums relation and (Lovesong, 1989, 5, Paris).  If we change the
+Rating field in the view from 5 to 3, we will need to change the
+latter tuple accordingly.  But notice that Lovesong also appears on
+another album, Galore, which doesn't show up in the view because its
+quantity is too low, and the functional dependency for the Tracks
+relation (visible in the annotations on the <tt>join_dl</tt> and
+<tt>select</tt> lenses) requires that all the tuples for a given song
+have the same rating.  The <tt>select</tt> lens ensures that this is
+the case by changing the rating in Galore's tuple to match the rating
+in Paris's tuple.  Try making this change and watch what happens.
+
+</ul>
+
 XXX;
 savedemo();
 # ---------------------------------------------------------
