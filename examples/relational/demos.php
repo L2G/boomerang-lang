7f160aa6db08346fc899e66882c8f25f9df304ba
Benjamin C. Pierce <bcpierce>
2006-06-23 02:36:57 +0000

    Final version (?) of relational demos for web

--- examples/relational/demos.php
+++ examples/relational/demos.php
@@ -8,15 +8,25 @@ $demo["r1format"] = $demo["r2format"] = "meta";
 $demo["forcer1"] = true;
 $demo["default_h"] = 200;
 $demo["r1_title"] = "Database";
+$demo["r1_h"] = 280;
 $demo["r2_title"] = "View";
+$demo["r2_h"] = 160;
 $demo["l1_title"] = "Lens";
 $demo["l1_d"] = true;
-$demo["l2_d"] = false;
+$demo["l1_h"] = 520;
+$demo["l2_d"] = true;
 $demo["l2_title"] = "(Not used)";
 $demo["r1_shows"] = true;
 $demo["l2"] = "id";
 $demo["output_d"] = "block";
 $demo["output_w"] = 450;
+$demo["output_h"] = 60;
+
+
+$demo["element_order"] = 
+    array("instr", "l1", "r1", "r2", "output");
+
+# "a2", "l2", "schema", "ar", "a1"
 
 # ---------------------------------------------------------
 $demo["instr"] = <<<XXX
@@ -144,20 +154,32 @@ savedemo();
 # ---------------------------------------------------------
 $demo["instr"] = <<<XXX
 
-<ul> <li> First, try changing the rating of Lovesong in the view.
+First, try changing the rating of Lovesong in the view.
 Press the Synchronize button and notice that the new rating gets
 propagaged back to the database.  (In this set of demos, where there
 is no second lens, "synchronize" just means "push the new value from
 the view back into the database.")
 
-<li> Next try changing the quantity of Trust in the view to 1.  Notice
+XXX;
+savedemo();
+# ---------------------------------------------------------
+$demo["instr"] = <<<XXX
+
+Next try changing the quantity of Trust in the view to 1.  Notice
 that the quantity gets propagated to the database.  
 Exercise: What do
 you think will happen if you try to change the quantity to 1?
 
-<li> Delete the tuple (Trust, 4, Wish, 5) from the view and
+XXX;
+savedemo();
+# ---------------------------------------------------------
+$demo["instr"] = <<<XXX
+
+Delete the tuple (Trust, 4, Wish, 5) from the view and
 synchronize.  Notice that the entry for Trust is removed from the
 Tracks database.  
+
+<p>
 
 We could also have achieved the deletion from the view by deleting the
 tuple (Wish, 5) from the Albums database; if this is the behavior we
@@ -167,7 +189,12 @@ left"--i.e., the first argument) so that '"Albums" with {Album ->
 Quantity}' comes before '"Tracks" with {Track -> Date, Track ->
 Rating}'.  Try this.
 
-<li> Finally, take a look at the tuple (Paris, 4, Lovesong, 5) in the
+XXX;
+savedemo();
+# ---------------------------------------------------------
+$demo["instr"] = <<<XXX
+
+Finally, take a look at the tuple (Paris, 4, Lovesong, 5) in the
 view.  The sources of this information are the tuples (Paris, 4) in
 the Albums relation and (Lovesong, 1989, 5, Paris).  If we change the
 Rating field in the view from 5 to 3, we will need to change the
@@ -180,9 +207,6 @@ have the same rating.  The <tt>select</tt> lens ensures that this is
 the case by changing the rating in Galore's tuple to match the rating
 in Paris's tuple.  Try making this change and watch what happens.
 
-</ul>
-
 XXX;
 savedemo();
-# ---------------------------------------------------------
 
