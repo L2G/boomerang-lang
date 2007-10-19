edecebfecda8cf78f80a15e86a4ff34dda132051
Nate Foster <jnfoster>
2007-10-19 05:47:44 +0000

    small improvements. better error handling, simple printing of actions with debug flag set

--- examples/boomison/main.ml
+++ examples/boomison/main.ml
@@ -1,6 +1,7 @@
 let sprintf = Printf.sprintf
 
-let name = "uniboom"
+let name = "boomison"
+
 let () = Util.supplyFileInUnisonDirFn (fun s -> sprintf "./.%s/%s" name s)
 
 let debug s = Trace.debug "uniboom" (fun () -> Util.format "%s" s)
@@ -18,54 +19,52 @@ let go l c_fn a_fn =
   match Sys.file_exists o_fn, Sys.file_exists c_fn, Sys.file_exists a_fn with 
     | _,false,false -> 
         (* if neither c nor a exists, clear o and return *)
-        Misc.remove_file_or_dir o_fn;
-        None
+        Misc.remove_file_or_dir o_fn
 
     | _,true,false -> 
         (* if c exists but a does not, set a to GET c *)
         cp c_fn o_fn;
         get c_fn a_fn;
-        None
+        debug (sprintf "%s -- get(%s) --> %s\n" c_fn l a_fn)
           
     | true,false,true ->
         (* if c does not exist but a and o do, set c to PUT a o *)        
         put a_fn o_fn c_fn;
         cp c_fn o_fn;
-        None
+        debug (sprintf "(%s,%s) -- put(%s) --> %s\n" a_fn o_fn l c_fn)
           
     | false,false,true ->
         (* if c and o do not exist but a does, set c to CRT a *)        
         crt a_fn c_fn;
         cp c_fn o_fn;
-        None          
+        debug (sprintf "%s -- create(%s) --> %s\n" a_fn l c_fn)
 
     | false,true,true -> 
         (* if c and a exist but o does not and a <> GET c then conflict *)
         let t_fn = tmp_fn a_fn in 
         get c_fn t_fn;
         if eq a_fn t_fn then
-          (cp c_fn o_fn; 
-           None)
+          cp c_fn o_fn
         else 
-          Some (c_fn, a_fn)
+          debug (sprintf "%s --> conflict <-- %s\n" c_fn a_fn)
+
     | true,true,true -> 
         (* otherwise, c, a, and o exist:
            - if c=o, set c to PUT a o
            - else if a=GET o set a to GET c
            - otherwise conflict *)
-        if eq c_fn o_fn then
+        let t_fn = tmp_fn a_fn in 
+        get o_fn t_fn;
+        if eq a_fn t_fn then 
+          (get c_fn a_fn;
+           cp c_fn o_fn;
+           debug (sprintf "%s -- get(%s) --> %s\n" c_fn l a_fn))
+        else if eq c_fn o_fn then 
           (put a_fn o_fn c_fn;
            cp c_fn o_fn;
-           None)
+           debug (sprintf "(%s,%s) -- put(%s) --> %s\n" a_fn o_fn l c_fn))
         else 
-          (let t_fn = tmp_fn a_fn in 
-             get o_fn t_fn;             
-            if eq a_fn t_fn then 
-              (get c_fn a_fn;
-               cp c_fn o_fn;                
-               None)
-            else 
-              Some (c_fn,a_fn))
+          debug (sprintf "%s --> conflict <-- %s\n" c_fn a_fn)
             
 let usage = sprintf "%s [options]" name
 
@@ -73,33 +72,34 @@ let l_pref = Prefs.createStringList "lens" "lens" "lens"
 let c_pref = Prefs.createStringList "concrete" "concrete" "abstract"
 let a_pref = Prefs.createStringList "abstract" "abstract" "abstract"
 
-let () = 
-  Prefs.profileName := Some "default";
+let profile_name = "default" 
+let profile_fn = Prefs.profilePathname profile_name 
+
+let main () = 
+  Prefs.profileName := Some profile_name;
   Prefs.parseCmdLine usage;
-  Prefs.loadTheFile ()
+  if not (Sys.file_exists profile_fn) then 
+    Error.simple_error (sprintf "Error: profile file %s does not exist" profile_fn)
+  else
+    Prefs.loadTheFile ();
 
-let ll = Prefs.read l_pref  
-let cl = Prefs.read c_pref  
-let al = Prefs.read a_pref  
-let ll_len = Safelist.length ll 
-let cl_len = Safelist.length cl 
-let al_len = Safelist.length al 
-let () = if cl_len <> al_len then 
-    Error.simple_error "number of concretes and abstracts differs"
+  let ll = Prefs.read l_pref in 
+  let cl = Prefs.read c_pref in
+  let al = Prefs.read a_pref in 
+  let ll_len = Safelist.length ll in 
+  let cl_len = Safelist.length cl in
+  let al_len = Safelist.length al in
+  if cl_len <> al_len then 
+    Error.simple_error "Error: number of concrete and abstract replicas differs"
   else if cl_len <> ll_len then 
-    Error.simple_error "number of files and lenses differs"
+    Error.simple_error "Error: number of replicas and lenses differs"
+      
+  let rec loop ll cl al = match ll,cl,al with 
+    | [],[],[] -> ()
+    | l::lrest,c::crest,a::arest -> 
+        go l c a;
+        loop lrest crest arest 
+    | _ -> assert false in 
+  loop ll cl al 
 
-let () = debug (sprintf "%d %d %d\n" ll_len cl_len al_len)
-
-let rec aux ll cl al acc = match ll,cl,al with 
-  | [],[],[] -> 
-      Safelist.rev acc
-  | l::lrest,c::crest,a::arest -> 
-      aux lrest crest arest ((go l c a)::acc)
-  | _ -> assert false 
-    
-let () = Safelist.iter
-  (function None -> ()
-    | Some (c_fn,a_fn) -> 
-        Util.format "Conflict on %s and %s\n" c_fn a_fn)
-    (aux ll cl al [])
+let () = Error.exit_on_error main
