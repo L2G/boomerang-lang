1a2559db88b03f27e153afbf8159ad33378b4fc3
Nate Foster <jnfoster>
2007-12-12 21:42:20 +0000

    More tidying and packaging.

--- html/index.php
+++ html/index.php
@@ -8,11 +8,9 @@
       <img src="images/harmony-header.png" alt="harmony-header"/>
 </td><td style="text-align:right; font-size:11pt; vertical-align:bottom;">
       [ <a href="#introduction">introduction</a> | 
-      <a href="#demo">demo</a> | 
-      <a href="#source">source</a> |
+      <a href="#distrib">distribution</a> | 
       <a href="#members">members</a> | 
       <a href="#papers">publications</a> | 
-      <a href="#related">related</a> | 
       <a href="#support">support</a> ] 
 </td></tr></table>
 </div>
@@ -20,54 +18,54 @@
 <a name="introduction"></a>
 <div class="content">
 <div class="title">Introduction <div class="top">[<a href="#top">Top</a>]</div></div>
-<p>The <strong>Harmony</strong> system is a generic framework for
-reconciling disconnected updates to heterogeneous, replicated XML
-data. It can be used, for instance, to synchronize the bookmark files
-of several different web browsers, allowing bookmarks and bookmark
-folders to be added, deleted, edited, and reorganized by different
-users running different browser applications on disconnected
-machines.</p>
 
-<p>A central theme of the Harmony project is bringing ideas from
+<p><strong>Harmony</strong> is framework for defining, updating, 
+    and synchronizing <i>views</i> over strings, trees, and relations. 
+It can be used, for example, to synchronize the bookmark files 
+of several different web browsers, to convert address books represented 
+in vCard to CSV, and to map between BiBTeX and RIS-formatted bibliographic
+databases.</p>
+
+<p>A central theme of the project is bringing ideas from
 programming languages to bear on a set of problems more commonly
-regarded as belonging to the purview of databases or distributed
-systems. In particular, a major component of the proposed work
-concerns developing the foundations of <em>bi-directional</em>
-programming languages, in which every program denotes a pair of
-functions---one for extracting a <em>view</em> of some complex data
-structure, and another for ``putting back'' an updated view into the
-original structure. Bi-directional programs play a crucial role in the
-way the system deals with heterogeneous structures, mapping between
-diverse concrete application data formats and common abstract formats
-suitable for synchronization. Similarly, the issue of
-<em>alignment</em> during reconciliation---that is, of determining
-which parts of divergent replicas are intended to represent ``the same
-information''---can be addressed by focusing on the type structure of
-the data being reconciled.</p>
+regarded as belonging to the purview of databases. Views are usually 
+discussed in the context of databases, but actually arise in a host 
+of applications across many diverse areas of computing.  In many of 
+these applications, is necessary to apply an update to the view, and 
+have the update be reflected back on the source. Unfortunately, the 
+view update problem remains largely unsolved; except in very simple 
+cases, views are read-only.  Our goal is to develop <em>programming 
+languages</em> in which programmers can specify both how to calculate 
+a view, and how to translate updates to a view back to the source, and
+to use these <em>bidirectional programs</em>&#x2014;called 
+<em>lenses</em>&#x2014;to build converters and synchronizers for a 
+variety of real-world data formats.</p>
 </div>
 
-<a name="demo"></a>
+<a name="distrib"></a>
 <div class="content">
-<div class="title">Demo<div class="top">[<a href="#top">Top</a>]</div></div>
+<div class="title">Distribution<div class="top">[<a href="#top">Top</a>]</div></div>
 <p>
-An extensive set of live demos and tutorials on the features of
-Harmony can be found 
-<a href="http://alliance.seas.upenn.edu/~harmony/cgi-bin/demo.php">here</a>.
+Harmony is free software; you can redistribute it and/or modify it
+under the terms of the GNU Lesser General Public License as published
+by the Free Software Foundation.
 </p>
-</div>
+<p>
+Harmony is composed of two largely distinct systems: 
+<ul>
+<li><strong>Boomerang:</strong> our current work, which focuses on lenses for string data.</li>
+<li><strong>Focal:</strong> older work on lenses and synchronizers for trees and relations.</li>
+</li>
+</ul></p>
 
-<a name="source"></a>
-<div class="content">
-<div class="title">Source distribution<div class="top">[<a href="#top">Top</a>]</div></div>
-<dl>
-<dt><h2>Snapshot tarballs</h2></dt>
-<dd>
+<h2>Binary Distribution</h2>
+<p>We offer pre-compiled binaries for Linux (x86), OS X (x86 and PPC), and Window (Cygwin):
 <table class="spaced" style="margin:7px 0px;">    
 <?php
 $dh = opendir("../download");
 $files = array();
 while($f = readdir($dh)) {
-  if(preg_match('/.tar.gz$/', $f)) { 
+  if(preg_match('/binary.tar.gz$/', $f)) { 
     $files[$f] = filemtime("../download/$f");
   }
  }
@@ -96,9 +94,48 @@ EOF;
 }
 ?>
 </table></dd>
+</p>
 
-<dt><h2>Subversion repository access</h2></dt>
-<dd>
+<h2>Source Distribution</h2>
+<p>
+<table class="spaced" style="margin:7px 0px;">    
+<?php
+$dh = opendir("../download");
+$files = array();
+while($f = readdir($dh)) {
+  if(preg_match('/source.tar.gz$/', $f)) { 
+    $files[$f] = filemtime("../download/$f");
+  }
+ }
+closedir($dh);
+arsort($files);
+
+$odd = true;
+foreach($files as $f=>$t) {
+  $size = hsize(filesize("../download/$f"));
+  $date = date("j M Y g:ia", $t);    
+  $trclass = "";
+  if($odd) {
+     $odd = false;
+     $trclass = " class=\"darkyellow\"";
+  } else {
+    $odd = true;
+    $trclass = " class=\"lightyellow\"";
+  }
+  print<<<EOF
+    <tr$trclass><td>
+      <a href="download/$f">
+      <img style="vertical-align:middle;border:0;" src="images/floppy.png" alt="floppy"/>
+      $f</a>
+      </td><td>$size</td><td style="text-align:right">$date</td></tr>
+EOF;
+}
+?>
+</table></dd>
+</p>
+
+<h2>Subversion Access</h2>
+<p>
   For anonymous, read-only access our <a href="http://subversion.tigris.org/">Subversion</a> repository, follow these steps:
   <ul>
   <li>Store the contents of <a href="harmony_key">this file</a> as <tt>~/.ssh/harmony_key</tt>.</li>
@@ -107,8 +144,12 @@ EOF;
   <li>Check out the repository: <tt>svn checkout svn+ssh://harmony@halfdome.cis.upenn.edu/trunk</tt>.</li>
   </ul>
   Commit access for frequent contributors can be arranged.
-</dd>
-</dl>
+</p>
+
+<h2>Online Demos</h2>
+An extensive set of live demos and tutorials on the features of Focal can be found 
+<a href="http://alliance.seas.upenn.edu/~harmony/cgi-bin/demo.php">here</a>.
+</p>
 </div>
 
 <table style="width:100%;">
@@ -121,9 +162,6 @@ EOF;
       <li><a href="http://www.cis.upenn.edu/~bohannon/">Aaron Bohannon</a></li>
       <li><a href="http://www.cis.upenn.edu/~jnfoster/">Nate Foster</a></li>
       <li><a href="http://www.cis.upenn.edu/~sanjeev">Sanjeev Khanna</a></li>
-    </ul>
-    </td><td>
-    <ul>
       <li><a href="http://www.cis.upenn.edu/~bcpierce/">Benjamin C. Pierce</a> (project leader)</li>      <li><a href="http://sardes.inrialpes.fr/~aschmitt/">Alan Schmitt</a></li>
     </ul>
     </td></tr></table>
@@ -137,13 +175,14 @@ EOF;
       <li><a href="http://www.cis.upenn.edu/~mbgreen">Michael Greenwald</a>
       <li>Owen Gunden</li>
       <li><a href="http://www.cis.upenn.edu/~kkunal">Keshav Kunal</a></li> 
-      <li><a href="http://www.seas.upenn.edu/~lescuyer">St&eacute;phane Lescuyer</a></li>
-      <li><a href="http://www.seas.upenn.edu/~vaughan2/">Jeff Vaughan</a></li>
+      <li><a href="http://www.lri.fr/~lescuyer/">St&eacute;phane Lescuyer</a></li>
     </ul>
     </td><td>
     <ul>
-      <li><a href="http://www.cis.upenn.edu/~jonm">Jon Moore</a></li>
-      <li><a href="http://www.cis.upenn.edu/~zheyang">Zhe Yang</a></li>
+      <li>Alexandre Pilkiewicz</li>
+      <li>Jon Moore</li>
+      <li><a href="http://www.seas.upenn.edu/~vaughan2/">Jeff Vaughan</a></li>
+      <li><a href="http://research.microsoft.com/users/zhey/">Zhe Yang</a></li>
     </ul>
     </td></tr></table>
   </div>
@@ -155,26 +194,6 @@ EOF;
 <h2>Papers</h2> <?php include("harmony-papers.html"); ?>
 <h2>Talks</h2> <?php include("harmony-talks.html"); ?>
 <h2>Miscellaneous</h2> <?php include("harmony-misc.html"); ?>
-</div>
-
-<a name="related"></a>
-<div class="content">
-<div class="title">Related projects<div class="top">[<a href="#top">Top</a>]</div></div>
-<ul>
-  <li> Many forms of synchronization can be found in present-day distributed systems: 
-    low-level OS support for distributed filesystems with optimistic replication, 
-    consistency protocols for replicated databases, middleware support for groupware 
-    applications, and user-level utilities for synchronizing PDAs with desktop 
-    databases and laptop filesystems with fileservers. Our earlier work in this 
-    area, in the <a href="http://www.cis.upenn.edu/~bcpierce/unison/">Unison</a> project, 
-    has focused on the latter category of tools, commonly called <i>file synchronizers</i>. 
-    The goal of the Harmony project is to to apply our experience with file synchronization 
-    to the related but more general domain of synchronizing arbitrary tree-structured 
-    data. Visit the <a href="http://www.cis.upenn.edu/~bcpierce/unison/">Unison home page</a> 
-    for more information.</li>
-  <li>We also maintain a (very incomplete) list of links to <a href="related.html">related 
-    projects</a> on synchronization.</li>
-</ul>
 </div>
 
 <a name="support"></a>
