4fc3d7b7fa9683965ca2b32f1c550b1345c95b82
Nate Foster <jnfoster>
2008-06-25 18:46:25 +0000

    o Fixed reporting of synchronizer actions.
       We could still be a little more verbose. Benjamin: I'd like to
       consult with you.

--- src/bsync.ml
+++ src/bsync.ml
@@ -29,30 +29,85 @@ module Diff3 = Bdiff3.Make(
   end)
   
 let debug thk = Trace.debug "sync+" thk  
-let report thk = Trace.debug "sync" thk
+let report thk = thk ()
 
-type dir = Left | Right | Both
+type act = Added | Deleted 
+
+type dir = A | B | Both
 
 let strings_of_dir = function
-  | Left  -> "the archive and replica B", "A"
-  | Right -> "the archive and replica A", "B"
+  | A  -> "the archive and replica B", "A"
+  | B -> "the archive and replica A", "B"
   | Both  -> "replicas A and B", "both"   
-
 
 type path_elt = Tag of Erx.tag | Key of Erx.key 
 
 let string_of_path_elt = function
-  | Tag t -> if t <> "" then ":" ^ t else t
-  | Key k -> "/" ^ k
+  | Tag t -> if t <> "" then ":" ^ (Misc.whack t) else (Misc.whack t)
+  | Key k -> "/" ^ (Misc.whack k)
 let root = []
+
 let string_of_path p = 
   if p = [] then "." 
   else Safelist.fold_left (fun acc pi -> string_of_path_elt pi ^ acc) "" p
 
+let keys_string kl = Misc.concat_list "," (Safelist.map (fun (ki,_) -> Misc.whack ki) kl)
+let tags_string ts = Misc.concat_list "," (Erx.TagSet.fold (fun ti acc -> Misc.whack ti::acc) ts [])
+
+let string_of_dir = function
+  | A     -> "A    "
+  | B     -> "B    "
+  | Both  -> "A & B" 
+
+let string_of_act = function
+  | Added   -> "added  "
+  | Deleted -> "deleted"
+
+(* reporting *)
+let report_spine_conflict spo spa spb p = 
+  report 
+    (fun () -> 
+       msg "@[Conflict on spines [%s] [%s] [%s] at [%s]@]@\n" 
+         (Erx.string_of_spine spo) (Erx.string_of_spine spa) (Erx.string_of_spine spb) (string_of_path p))
+
+let report_tags_action r a ts p = 
+  Erx.TagSet.iter 
+    (fun ti -> 
+       report 
+         (fun () -> 
+            msg "@[%s %s tag at [%s]@]@\n" (string_of_dir r) (string_of_act a) (string_of_path (Tag ti::p))))
+    ts
+
+let report_tags_del_mod r rb tso tsr tsrb p = 
+  report 
+    (fun () -> 
+       msg "@[Delete(%s) / modify (%s) conflict on tags {%s} {%s} {%s} at [%s].@]@\n"
+         r rb (tags_string tso) (tags_string tsr) (tags_string tsrb) (string_of_path p))
+
+let report_keys_action r a kl p = 
+  Safelist.iter 
+    (fun (ki,_) -> 
+       report 
+         (fun () -> 
+            msg "@[%s %s key at [%s]@]@\n" (string_of_dir r) (string_of_act a) (string_of_path (Key ki::p))))
+    kl
+
+let report_keys_del_mod r rb okl rkl rbkl p = 
+  report 
+    (fun () -> 
+       msg "@[Delete(%s) / modify (%s) conflict on keys [%s] [%s] [%s] at [%s].@]@\n"
+         r rb (keys_string okl) (keys_string rkl) (keys_string rbkl) (string_of_path p))
+
+let report_keys_conflict okl akl bkl p = 
+  report 
+    (fun () -> 
+       msg "@[Conflict on keys [%s] [%s] [%s] at [%s].@]@\n"
+       (keys_string okl) (keys_string akl) (keys_string bkl) (string_of_path p))
+
 let atomic_sync o a b = 
   if a=b then Some(a,Both)
-  else if o=b then Some(a,Left)
-  else if o=a then Some(b,Right)
+  else if o=b then Some(a,A)
+  else if o=a then Some(b,B)
   else None
 
 let rec isync p ty o a b = 
@@ -62,19 +117,19 @@ let rec isync p ty o a b =
   let o',a',b' = 
     begin match atomic_sync spo spa spb with
       | None -> 
-          report 
-            (fun () -> 
-               msg "@[Conflict at [%s]:" (string_of_path p);
-               msg " returning the archive and replicas unchanged.@]@\n");
+          report_spine_conflict spo spa spb p;
           (o,a,b)
       | Some(s,dir) -> begin
           let s_tags = Erx.spine_tags s in
+          let o_tags = Erx.spine_tags spo in 
           let a_tags = Erx.spine_tags spa in 
           let b_tags = Erx.spine_tags spb in 
           let r,r_tags,tmr,rb,rb_tags,tmrb = 
-            if dir = Left then "A",a_tags,tma,"B",b_tags,tmb
+            if dir = A then "A",a_tags,tma,"B",b_tags,tmb
             else "B",b_tags,tmb,"A",a_tags,tma in
-          let del_tags = Erx.TagSet.diff rb_tags s_tags in 
+          (* compute deletes / modified tags *)
+          let del_tags = Erx.TagSet.diff rb_tags s_tags in  
+          let add_tags = Erx.TagSet.diff r_tags s_tags in 
           let mod_tags = 
             Erx.TagSet.filter 
               (fun ti -> Erx.TagMap.safe_find ti tmrb [] <> Erx.TagMap.safe_find ti tmo [])
@@ -83,23 +138,13 @@ let rec isync p ty o a b =
           (* check for delete / modify conflict *)
             if not (Erx.TagSet.is_empty del_mod_tags) then 
               begin 
-                let ti = Erx.TagSet.choose del_mod_tags in 
-                report 
-                  (fun () -> 
-                     msg "@[Delete(%s) / modify(%s) conflict on tag %s @ [%s].@]@\n"
-                       r rb (Misc.whack ti) (string_of_path p));
+                report_tags_del_mod r rb o_tags r_tags rb_tags p;
                 (o,a,b)
               end
-            else       
-              if Erx.TagSet.is_empty s_tags then 
-                begin
-                  let s1,s2 = strings_of_dir dir in 
-                  if dir <> Both || o <> a then 
-                    report (fun () -> msg "@[Propagating %s at [%s].@]@\n" s2 (string_of_path p));
-                  let w = Erx.unparse (s,Erx.TagMap.empty) in
-                  (w,w,w)
-                end
-              else
+            else
+              begin
+                report_tags_action dir Deleted del_tags p;
+                report_tags_action dir Added add_tags p;
                 let tmo,tma,tmb =
                   Erx.TagSet.fold
                     (fun t (tmo,tma,tmb) ->
@@ -108,7 +153,6 @@ let rec isync p ty o a b =
                        let at = Erx.box_content ska t in 
                        let bt = Erx.box_content skb t in 
                        let chunks = Diff3.parse ot at bt in
-                       let key_string ri = Misc.concat_list "," (Safelist.map fst ri) in 
                        let ot',at',bt' =
                          Safelist.fold_left
                            (fun (ot',at',bt') chunk ->
@@ -118,47 +162,41 @@ let rec isync p ty o a b =
                                       | None -> assert false
                                       | Some ty' -> ty' in 
                                     let oi',ai',bi' = isync (Key k::pt) ty_t oi ai bi in
-                                    (ot'@[k,oi'],at'@[k,ai'],bt'@[k,bi'])
+                                      (ot'@[k,oi'],at'@[k,ai'],bt'@[k,bi'])
                                 | Diff3.AChange(oti,ati,bti) ->
                                     if oti = bti then 
                                       begin
-                                        report (fun () -> msg "@[Propagating A at [%s].@]@\n" (string_of_path p));
+                                        report_keys_action A Deleted oti pt;
+                                        report_keys_action A Added ati pt;
                                         (ot'@ati,at'@ati,bt'@ati)
-                                      end
+                                      end                            
                                     else 
-                                      begin 
-                                        report 
-                                          (fun () -> 
-                                             msg "@[Delete(%s) / modify (%s) conflict on keys [%s] [%s] [%s] at [%s].@]@\n"
-                                               r rb (key_string oti) (key_string ati) (key_string bti) (string_of_path pt));
+                                      begin                                         
+                                        report_keys_del_mod r rb oti ati bti pt;
                                         (ot'@oti,at'@ati,bt'@bti)
                                       end
                                 | Diff3.BChange(oti,ati,bti) ->
                                     if oti = ati then 
                                       begin
-                                        report (fun () -> msg "@[Propagating B at [%s].@]@\n" (string_of_path p));
+                                        report_keys_action B Deleted oti pt;
+                                        report_keys_action B Added bti pt;
                                         (ot'@bti,at'@bti,bt'@bti)
-                                      end
+                                      end                            
                                     else 
-                                      begin 
-                                        report 
-                                          (fun () -> 
-                                             msg "@[Delete(%s) / modify (%s) conflict on keys [%s] [%s] [%s] at [%s].@]@\n"
-                                               r rb (key_string oti) (key_string ati) (key_string bti) (string_of_path pt));
+                                      begin                                         
+                                        report_keys_del_mod r rb oti ati bti pt;
                                         (ot'@oti,at'@ati,bt'@bti)
                                       end
                                 | Diff3.Conflict(oti,ati,bti) ->
                                     if ati=bti then 
                                       begin 
-                                        if oti <> ati then report (fun () -> msg "@[Propagating both at [%s].@]@\n" (string_of_path p));
+                                        report_keys_action Both Deleted oti pt;
+                                        report_keys_action Both Added   ati pt;
                                         (ot'@ati,at'@ati,bt'@ati)
                                       end
                                     else
                                       begin
-                                        report 
-                                          (fun () -> 
-                                             msg "@[Conflict on keys [%s] [%s] [%s] at [%s].@]@\n"
-                                               (key_string oti) (key_string ati) (key_string bti) (string_of_path pt));                       
+                                        report_keys_conflict oti ati bti pt;
                                         (ot'@oti,at'@ati,bt'@bti)
                                       end)
                            ([],[],[]) chunks in
@@ -170,10 +208,10 @@ let rec isync p ty o a b =
                   (Erx.unparse (s,tmo),
                    Erx.unparse (s,tma),
                    Erx.unparse (s,tmb))
-        end                   
+              end                   
+        end 
     end in 
-    (o',a',b')
-      
+  (o',a',b')
       
 let sync t oo ao bo = 
   (* validate archive / replicas *)
@@ -207,9 +245,6 @@ let sync t oo ao bo =
                      (string_of_path root));
               (oo,ao,bo)
           | Some (r,d) -> 
-              (match oo,ao with 
-                 | Some o,Some a -> if o <> a then report (fun () -> msg "@[Propagating both at [%s].@]@\n" (string_of_path root))
-                 | _ -> ());
               (r,r,r)
         end
     | Some o,Some a,Some b ->
