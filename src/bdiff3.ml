c99e7e43895c59bae30bb7ca9c75c35478be48af
Larry Gilbert <larry@l2g.to>
2014-03-14 23:11:51 -0700

    Replace deprecated 'or'

--- src/bdiff3.ml
+++ src/bdiff3.ml
@@ -83,7 +83,7 @@ module Make(A: Diff3Arg) = struct
            lcs_oa.(i)) 
       lcs_oa;
     let rec find_matches i j l = 
-      if (i=0) or (j=0) then l
+      if (i=0) || (j=0) then l
       else match lcs_aux.(i).(j) with  
         | Diag -> find_matches (i-1) (j-1) ((i-1,j-1)::l)  
         | Top  -> find_matches (i-1) j l    
